b0e42bac7c1294fa6bd7e7c48f8e7f71cf5bde07
Emanuel Tavecia <emanueltavecia@hotmail.com>
2023-10-29 23:28:44 -0300

    testing angular speed

--- interpreter-with-library.ino
+++ interpreter-with-library.ino
@@ -5,9 +5,15 @@ const int stepsPerRevolution = 200;
 Stepper motor1(stepsPerRevolution, 2, 3);
 Stepper motor2(stepsPerRevolution, 4, 5);
 
+const int radius = 34;
+const int b = 250;
+float rightWheelSpeed;
+float leftWheelSpeed;
+
 String userInput;
 char codeLetter;
-int codeNumber;
+int linear;
+int angular;
 
 int currentVelocity = 300;
 
@@ -18,30 +24,60 @@ void setup() {
 }
 
 void loop() {
+  linear = 0;
+  angular = 0;
+
   Serial.println("Enter a code (options: v, f, b):");
   
   while (Serial.available() == 0) {}
 
-  userInput = Serial.readStringUntil('\n');
-  codeLetter = toupper(userInput.charAt(0));
-  codeNumber = userInput.substring(1).toInt();
+  userInput = Serial.readString();
+
+  userInput.toUpperCase();
+
+  char *str = (char *)userInput.c_str();
+  char *token = strtok(str, ",");
+  
+  if (token != NULL) {
+    codeLetter = token[0];
+    token = strtok(NULL, ",");
+    if (token != NULL) {
+      linear = atoi(token);
+      token = strtok(NULL, ",");
+      if (token != NULL) {
+        angular = atoi(token);
+      }
+    }
+  }
 
   if (codeLetter == 'V') {
-    motor1.setSpeed(codeNumber);
-    motor2.setSpeed(codeNumber);
+    if (angular == 0) {
+      motor1.setSpeed(linear * 9.549);
+      motor2.setSpeed(linear * 9.549);
+    } else {
+      rightWheelSpeed = (linear / b) + ((b / (2 * radius)) * angular);
+      leftWheelSpeed = (linear / b) - ((b / (2 * radius)) * angular);
+
+      motor1.setSpeed(rightWheelSpeed * 9.549);
+      motor2.setSpeed(leftWheelSpeed * 9.549);
+    }
   } else if (codeLetter == 'F') {
-    for(int i = 0; i < codeNumber; i++) {
+    for(int i = 0; i < linear; i++) {
       for(int i = 0; i < 800; i++) {
         motor1.step(1);
         motor2.step(1);
       }
+        Serial.println("step");
     }
   } else if (codeLetter == 'B') {
-    for(int i = 0; i < codeNumber; i++) {
+    for(int i = 0; i < linear; i++) {
       for(int i = 0; i < 800; i++) {
         motor1.step(-1);
         motor2.step(-1);
       }
+        Serial.println("step");
     }
+  } else {
+    Serial.println("Invalid code.");
   }
 }
